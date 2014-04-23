42a28aa41f435cd3ecc8dc2baf011106a472af73
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-04-23 21:40:55 +0100

    Upped the version in an attempt to successfully install oracle java.

--- attributes/java.rb
+++ attributes/java.rb
@@ -1,2 +1,3 @@
-override["java"]["install_flavor"] = "oracle"
-override["java"]["oracle"]["accept_oracle_download_terms"] = true
+node["java"]["install_flavor"] = "oracle"
+node["java"]["jdk_version"] = "7"
+node["java"]["oracle"]["accept_oracle_download_terms"] = true
