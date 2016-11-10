67225cb5349f00b44bf415328012e161057a5124
Fred Thompson <fred.thompson@buildempire.co.uk>
2016-11-10 20:07:27 +0000

    Adding systemd as a service installation option. By default, this is false, and the old method is used.
    Set node[:teamcity_build_agent][:systemd]=true to use.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
 default[:teamcity_build_agent][:server_url] = "http://teamcity.localhost"
+
+# A new property added that allows the service to be added as a systemd service instead,
+# which seem to be the preferred way in Ubuntu 16.04.
+default[:teamcity_build_agent][:systemd] = false
--- recipes/service.rb
+++ recipes/service.rb
@@ -6,18 +6,26 @@
 # Set the useful variables for the recipe
 agent_path = "/opt/teamcity/buildAgent"
 
-
-# Create TeamCity Service
-template '/etc/init/teamcity-build-agent.conf' do
-  source 'teamcity-build-agent.conf.erb'
-  variables(
-    :agent_path => agent_path
-  )
-  notifies :start, 'service[teamcity-build-agent]', :immediately
-end
-
-# Start TeamCity Service
-service "teamcity-build-agent" do
-  provider Chef::Provider::Service::Upstart
-  action :restart
+if node[:teamcity_build_agent][:systemd]
+  # Create TeamCity Service as systemd
+  systemd_unit "teamcity-build-agent.service" do
+    enabled true
+    active true
+    content "[Unit]\nDescription=TeamCity Build Agent\nAfter=network.target\n\n[Service]\nType=forking\nPIDFile=%{agent_path}/logs/buildAgent.pid\nExecStart=/usr/bin/sudo -u teamcity %{agent_path}/bin/agent.sh start\nExecStop=/usr/bin/sudo -u teamcity %{agent_path}/bin/agent.sh stop\n\nRestart=always\n[Install]\nWantedBy=multi-user.target"
+    action [:create, :enable, :start]
+  end
+else
+  # Create TeamCity Service
+  template '/etc/init/teamcity-build-agent.conf' do
+    source 'teamcity-build-agent.conf.erb'
+    variables(
+      :agent_path => agent_path
+    )
+    notifies :start, 'service[teamcity-build-agent]', :immediately
+  end
+  # Start TeamCity Service
+  service "teamcity-build-agent" do
+    provider Chef::Provider::Service::Upstart
+    action :restart
+  end
 end
